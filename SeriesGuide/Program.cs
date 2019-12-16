aae3394f30823147868c2bf8cfab4b8452425e32
Natalia Budanova <natabu2000@outlook.com>
2019-12-16 23:34:42 +0300

    Начало работы.

---
+++ SeriesGuide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeriesGuide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
